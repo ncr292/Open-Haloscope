04d079c06923ce689a2d8d42a600829a87c3d463
ncr292 <nicolo.crescini@gmail.com>
2023-05-23 01:05:59 +0200

    accelerometer added on .ino

--- lib/arduino/red_arduino_comm/red_arduino_comm.ino
+++ lib/arduino/red_arduino_comm/red_arduino_comm.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
 #define BMP_SCK  (13)
 #define BMP_MISO (12)
@@ -14,26 +16,41 @@ int BFieldPin = 1;
 //Adafruit_BMP280 bmp; // I2C
 Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK); // SPI
 
+//Adafruit ADXL345 Accelerometer // SCL, SDA
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);       // initialise UART with baud rate of 9600 bps
-  Serial.setTimeout(5);   // timeout to wait all the string to arrive
+  Serial.setTimeout(5);     // timeout to wait all the string to arrive
+  Wire.begin();
+
   bmp.begin();              // initialise the BMP280
+  accel.begin();            // initialise the ADXL345
+  accel.setRange(ADXL345_RANGE_4_G);
 }
 
-//double readLight() {
-//}
 
 void loop() {
 
   if (Serial.available()) {
     char data_rcvd = Serial.read();   // read one byte from serial buffer and save to data_rcvd
 
+    // get data from accelerometer
+    sensors_event_t event; 
+    accel.getEvent(&event);
+
     if (data_rcvd == 'p') Serial.println(bmp.readPressure()); // return pressure
     if (data_rcvd == 't') Serial.println(bmp.readTemperature()); // return temperature
     if (data_rcvd == 'l') Serial.println(analogRead(LightPin)); // return light
     if (data_rcvd == 'b') Serial.println(analogRead(BFieldPin)); // return B-field
+  
+    if (data_rcvd == 'x') Serial.println(event.acceleration.x); // return acceleration x
+    if (data_rcvd == 'y') Serial.println(event.acceleration.y); // return acceleration y
+    if (data_rcvd == 'z') Serial.println(event.acceleration.z); // return acceleration z
+
 
     digitalWrite(LED_BUILTIN, HIGH); // turn on the led 
     delay(100);
